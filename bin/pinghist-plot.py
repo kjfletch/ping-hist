ae4683eb921f6eb62b95cb53d8ef33fbafe0ec0d
Kevin John Fletcher <kjfletch@devbox.(none)>
2010-11-22 22:05:50 +0000

    Only plots loss if loss was recorded.

--- bin/pinghist-plot.py
+++ bin/pinghist-plot.py
@@ -14,13 +14,19 @@ def main():
         valid, data, lowestping, highestping, lowestloss, highestloss = scan_data(input_file)
 
         if not valid:
-            print 'Error: file "%s" has invalid data.' % (input_file)
-            sys.exit(-1)
+            print 'Warning: file "%s" has invalid data.' % (input_file)
+            continue
+
+        if len(data) <= 1:
+            print 'Warning: not enough data in file "%s" to plot.' % (input_file)
+            continue
 
         base, ext = os.path.splitext(input_file)
         output_file = base + '.png'
 
-        plot_data = get_plot_data(input_file, output_file)
+        plot_loss = highestloss != lowestloss
+
+        plot_data = get_plot_data(input_file, output_file, plot_loss)
         plot_cmd = shlex.split('gnuplot')
         plot = Popen(plot_cmd, stdin=PIPE)
         plot.communicate(plot_data)
@@ -91,8 +97,12 @@ def scan_data(input_file):
     valid = valid and lowping != None and highping != None and  lowloss != None and highloss != None
     return valid, data, lowping, highping, lowloss, highloss
 
-def get_plot_data(input_filename, output_filename):
-    data = plot_data_raw
+def get_plot_data(input_filename, output_filename, plot_loss=True):
+    if plot_loss:
+        data = plot_data_raw
+    else:
+        data = plot_data_no_loss_raw
+
     data = data.replace('__OUTPUT_FILENAME__', output_filename)
     data = data.replace('__INPUT_FILENAME__', input_filename)
 
@@ -115,18 +125,34 @@ set multiplot
 # Loss
 set origin 0.0,0.0
 set size 1,0.5
-set yrange [0 : 100]
 set ylabel "Packet Loss (%)"
 set format x "%d/%m %H:%M:%S"
 plot "__INPUT_FILENAME__" using 1:5 with lines title "Loss"
-unset yrange
 
 # Min, Max & Avg
-set autoscale
 set origin 0.0,0.5
 set size 1,0.5
 unset xlabel
 set format x ""
+set ylabel "Ping Time (ms)"
+
+plot "__INPUT_FILENAME__" using 1:3 with lines title "Max", \\
+     "__INPUT_FILENAME__" using 1:4 with lines title "Avg", \\
+     "__INPUT_FILENAME__" using 1:2 with lines title "Min"
+"""
+
+plot_data_no_loss_raw = """
+set terminal png
+set output "__OUTPUT_FILENAME__"
+set datafile separator ","
+set grid
+set timefmt "%Y/%m/%d %H:%M:%S"
+set xtics rotate by 90
+set xdata time
+set xlabel "Time" offset 0,-1
+set format x "%d/%m %H:%M:%S"
+
+# Min, Max & Avg
 set ylabel "Ping Time (ms)"
 
 plot "__INPUT_FILENAME__" using 1:3 with lines title "Max", \\
